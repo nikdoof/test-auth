3c90e7d5e13112d04b68d7805a2dd13baf307a57
Andrew Williams <andy@tensixtyone.com>
2010-02-25 18:55:37 +0100

    Added templates location

--- settings.py
+++ settings.py
@@ -66,9 +66,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'login.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 INSTALLED_APPS = (
@@ -78,6 +76,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'registration',
+    'eve_proxy',
+    'eve_api',
     'sso',
 )
 
