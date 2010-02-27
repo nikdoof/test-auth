942ed499582f38ef59cdb3548c70c10bf393b7ad
Andrew Williams <andy@tensixtyone.com>
2010-02-27 18:40:59 +0100

    Force lowercase usernames

--- sso/services/jabber/__init__.py
+++ sso/services/jabber/__init__.py
@@ -11,7 +11,7 @@ class JabberService(BaseService):
 
     def add_user(self, username, password):
         """ Add user to service """
-        return self.ejctl.register(username, settings.JABBER_SERVER, password)
+        return self.ejctl.register(username.lower(), settings.JABBER_SERVER, password)
 
     def set_corp(self, username):
         """ User is in corp, enable extra privs """
@@ -19,19 +19,19 @@ class JabberService(BaseService):
 
     def delete_user(self, username):
         """ Delete a user """
-        return self.ejctl.unregister(username, settings.JABBER_SERVER)
+        return self.ejctl.unregister(username.lower(), settings.JABBER_SERVER)
 
     def disable_user(self, username):
         """ Disable a user """
-        return self.ejctl.ban_user(settings.JABBER_SERVER, username)
+        return self.ejctl.ban_user(settings.JABBER_SERVER, username.lower())
 
     def enable_user(self, username):
         """ Enable a user """
-        return self.ejctl.enable_user(settings.JABBER_SERVER, username, password)
+        return self.ejctl.enable_user(settings.JABBER_SERVER, username.lower(), password)
 
     def check_user(self, username):
         """ Check if the username exists """
-        if username not in self.ejctl.get_users(settings.JABBER_SERVER):
+        if username.lower() not in self.ejctl.get_users(settings.JABBER_SERVER):
             return False
         else:
             return True
