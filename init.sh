e65afef1dbb5b3966787fb9ce42469d1035f2999
Andrew Williams <andy@tensixtyone.com>
2010-03-09 22:09:37 +0000

    Added django_evolution to the stack

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 
 svn checkout http://django-registration.googlecode.com/svn/trunk/registration
 svn co http://code.djangoproject.com/svn/django/branches/releases/1.1.X/django django
+svn co http://django-evolution.googlecode.com/svn/trunk/django_evolution
--- settings.py
+++ settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django_evolution',
     'registration',
     'django_cron',
     'eve_proxy',
