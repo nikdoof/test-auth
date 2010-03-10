c0d05dac8d960659ef4aab65dc4bd8d6d416661d
Andrew Williams <andy@tensixtyone.com>
2010-03-10 01:32:02 +0000

    Fixes eve_proxy to allow POST values

--- eve_proxy/views.py
+++ eve_proxy/views.py
@@ -10,7 +10,13 @@ def retrieve_xml(request):
     # This is the URL path (minus the parameters).
     url_path = request.META['PATH_INFO'].replace(reverse('eve_proxy.views.retrieve_xml'),"/")
     # The parameters attached to the end of the URL path.
-    params = request.META['QUERY_STRING']
+
+    if request.method == 'POST':
+        params = request.POST.urlencode()
+        print params
+    else:
+        params = request.META['QUERY_STRING']
+        print params
 
     if url_path == '/' or url_path == '':
         # If they don't provide any kind of query, shoot a quick error message.
