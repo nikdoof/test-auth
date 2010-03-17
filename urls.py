65a6e3155bc69389f4633c71c7f93fca4e7b8d4c
Andrew Williams <andy@tensixtyone.com>
2010-03-17 00:24:54 +0000

    Registration now uses unique email form, stops multiple registrations, fixes #16

--- urls.py
+++ urls.py
@@ -4,17 +4,20 @@ from django.contrib.auth.views import login
 import django_cron
 import settings
 
+from registration.views import register
+from registration.forms import RegistrationFormUniqueEmail
+
 django_cron.autodiscover()
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^register/$', register, {'form_class' :RegistrationFormUniqueEmail }),
     (r'^admin/', include(admin.site.urls)),
     ('', include('registration.urls')),
     ('', include('sso.urls')),
     (r'^eveapi/', include('eve_proxy.urls')),
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    )
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+)
