1a6fd720d3e75603943480d80739d1c32fab09b5
Andrew Williams <andy@tensixtyone.com>
2010-03-23 14:50:50 +0000

    Fixed evolution

--- mumble/evolutions/mumble-db-update-1.py
+++ mumble/evolutions/mumble-db-update-1.py
@@ -3,7 +3,7 @@ from django.db import models
 
 MUTATIONS = [
     AddField('Mumble', 'display', models.CharField, initial='', max_length=200),
-    AddField('Mumble', 'server', models.ForeignKey, initial=<<USER VALUE REQUIRED>>, related_model='mumble.MumbleServer'),
+    AddField('Mumble', 'server', models.ForeignKey, initial='<<USER VALUE REQUIRED>>', related_model='mumble.MumbleServer'),
     DeleteField('Mumble', 'dbus'),
     ChangeField('Mumble', 'port', initial=None, null=True)
 ]
