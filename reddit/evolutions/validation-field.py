be12cbf90afd18ee58bc1d91fe462f7b08c83760
Andrew Williams <andy@tensixtyone.com>
2010-04-02 19:40:01 +0100

    Added initial value to evolution

--- reddit/evolutions/validation-field.py
+++ reddit/evolutions/validation-field.py
@@ -2,6 +2,6 @@ from django_evolution.mutations import *
 from django.db import models
 
 MUTATIONS = [
-    AddField('RedditAccount', 'validated', models.BooleanField),
+    AddField('RedditAccount', 'validated', models.BooleanField, initial=False),
 ]
 
