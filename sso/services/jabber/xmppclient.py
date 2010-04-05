56ce23a186e518780a12107c3bb73c815d697ff3
Andrew Williams <andy@tensixtyone.com>
2010-04-05 22:22:36 +0100

    Fixed jabber disableuser function

--- sso/services/jabber/xmppclient.py
+++ sso/services/jabber/xmppclient.py
@@ -127,7 +127,7 @@ class JabberAdmin():
         except:
             return False
 
-        if self.resetpassword(username, hashlib.sha1('%s%s%s' % (username, random.randint(0, 2147483647))).hexdigest()):
+        if self.resetpassword(username, hashlib.sha1('%s%s' % (username, random.randint(0, 2147483647))).hexdigest()):
             return self.kickuser(username)
         else:
             return False
