975b20b5d40aa605d183b91ca94de34d25ce296a
Andrew Williams <andy@tensixtyone.com>
2010-04-08 10:00:56 +0100

    Removed registration from the init script

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-svn checkout http://django-registration.googlecode.com/svn/trunk/registration
 svn co http://code.djangoproject.com/svn/django/branches/releases/1.1.X/django django
 svn co http://django-evolution.googlecode.com/svn/trunk/django_evolution
