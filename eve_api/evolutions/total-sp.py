a2a32f398a32f82018e404fab70aa28a4e99d9d8
Andrew Williams <andy@tensixtyone.com>
2010-04-20 14:09:01 +0100

    Updated evolution

--- eve_api/evolutions/total-sp.py
+++ eve_api/evolutions/total-sp.py
@@ -2,6 +2,6 @@ from django_evolution.mutations import *
 from django.db import models
 
 MUTATIONS = [
-    AddField('EVEPlayerCharacter', 'total_sp', models.IntegerField, initial=0)
+    AddField('EVEPlayerCharacter', 'total_sp', models.IntegerField, null=True)
 ]
 
