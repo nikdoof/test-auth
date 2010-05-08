c798e33e23c4222da9ef5ba7385caabf773298ed
Andrew Williams <andy@tensixtyone.com>
2010-05-09 00:30:40 +0100

    Added some useful fields to the HR admin interface

--- hr/admin.py
+++ hr/admin.py
@@ -4,8 +4,13 @@ from django.contrib.auth.admin import UserAdmin
 from hr.models import Application, Recommendation, Audit
 
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'character', 'status')
+    list_display = ('user', 'character', 'status', 'recommendations')
     search_fields = ['user', 'character', 'status']
+
+    def recommendations(self, obj):
+        return len(obj.recommendation_set.all())
+
+    recommendations.short_description = '# of Recommendations'
 
     def save_model(self, request, obj, form, change):
         obj.save(user=request.user)
@@ -13,7 +18,7 @@ class ApplicationAdmin(admin.ModelAdmin):
 admin.site.register(Application, ApplicationAdmin)
 
 class RecommendationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'user_character')
+    list_display = ('user', 'user_character', 'application')
     search_fields = ['user_character']
 
 admin.site.register(Recommendation, RecommendationAdmin)
