a04d975ffa0b7cff6f3e7ff1e3a23de052f2a92f
Andrew Williams <andy@tensixtyone.com>
2010-05-11 13:05:26 +0100

    Switched jabber service over to in-model config

--- sso/services/jabber/__init__.py
+++ sso/services/jabber/__init__.py
@@ -7,12 +7,14 @@ class JabberService(BaseService):
 
     settings = { 'require_user': True,
                  'require_password': True,
-                 'provide_login': False }
+                 'provide_login': False,
+                 'jabber_server': 'dredd.it',
+                 'jabber_xmlrpc_url': 'http://127.0.0.1:4560' }
 
     @staticmethod
     def exec_xmlrpc(func, **kwargs):
         """ Send a XMLRPC request """
-        server = xmlrpclib.Server(settings.JABBER_XMLRPC_URL)
+        server = xmlrpclib.Server(self.settings['jabber_xmlrpc_url'])
         params = {}
         for i in kwargs:
             params[i] = kwargs[i]
@@ -21,11 +23,11 @@ class JabberService(BaseService):
 
     def add_user(self, username, password, **kwargs):
         """ Add user to service """
-        res = self.exec_xmlrpc('register', user=username, host=settings.JABBER_SERVER, password=password)
+        res = self.exec_xmlrpc('register', user=username, host=self.settings['jabber_server'], password=password)
         if res['res'] == 0:
             if 'character' in kwargs:
-                self.exec_xmlrpc('set_nickname', user=username, host=settings.JABBER_SERVER, nickname=kwargs['character'].name)
-            return "%s@%s" % (username, settings.JABBER_SERVER)
+                self.exec_xmlrpc('set_nickname', user=username, host=self.settings['jabber_server'], nickname=kwargs['character'].name)
+            return "%s@%s" % (username, self.settings['jabber_server'])
         else:
             return False
 
@@ -40,7 +42,7 @@ class JabberService(BaseService):
 
     def check_user(self, username):
         """ Check if the username exists """
-        res = self.exec_xmlrpc('check_account', user=username, host=settings.JABBER_SERVER)
+        res = self.exec_xmlrpc('check_account', user=username, host=self.settings['jabber_server'])
         if res['res'] == 0:
             return True
         else:
