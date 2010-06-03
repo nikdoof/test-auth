022feb3d4b07c3ad3576a942a44677519810d3ba
Andrew Williams <andy@tensixtyone.com>
2010-06-03 23:55:32 +0100

    Updated init and start scripts

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/python ./manage.py runfcgi method=threaded host=127.0.0.1 port=9981 pidfile=./auth.pid
+/usr/bin/python ./manage.py runfcgi method=threaded host=127.0.0.1 port=9981
