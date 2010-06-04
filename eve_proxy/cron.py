7e094f3a37ceafea6f9352d034831d56412c57b5
Andrew Williams <andy@tensixtyone.com>
2010-06-04 09:00:51 +0100

    Added FlushCache cronjob, also fixed output message for ClearStaleCache

--- eve_proxy/cron.py
+++ eve_proxy/cron.py
@@ -16,5 +16,22 @@ class ClearStaleCache():
                 
         def job(self):
             objs = CachedDocument.objects.filter(cached_until__lt=datetime.utcnow())
-            self._logger.info('Removing %s stale cache documents')
+            self._logger.info('Removing %s stale cache documents' % len(objs))
             objs.delete()
+
+class FlushCache():
+        """
+        Flushes the entire cache
+        """
+
+        @property
+        def _logger(self):
+            if not hasattr(self, '__logger'):
+                self.__logger = logging.getLogger(__name__)
+            return self.__logger
+                
+        def job(self):
+            objs = CachedDocument.objects.all()
+            self._logger.info('Removing %s stale cache documents' % len(objs))
+            objs.delete()
+
