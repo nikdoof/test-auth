8a1df3fa688d96dc1caa842912f60a4bf9fae61a
Andrew Williams <andy@tensixtyone.com>
2010-06-10 13:39:27 +0100

    Now checks for logged in user

--- api/handlers.py
+++ api/handlers.py
@@ -17,18 +17,18 @@ class UserHandler(BaseHandler):
                 u = User.objects.get(id=id)
             except (User.DoesNotExist, ValueError):
                 return rc.NOT_HERE
-
-        if 'user' in request.GET:
+        elif 'user' in request.GET:
             try:
                 u = User.objects.get(username=request.GET['user'])
             except User.DoesNotExist:
                 return rc.NOT_HERE
-
-        if 'serviceuid' in request.GET:
+        elif 'serviceuid' in request.GET:
             try:
                 u = ServiceAccount.objects.get(service_uid=request.get['serviceuid']).user
             except ServiceAccount.DoesNotExist:
                 return rc.NOT_HERE
+        elif request.user:
+            u = request.user
 
         chars = []
         for a in u.eveaccount_set.all():
