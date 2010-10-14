f6e886969352a858eb2bc803f54cd2dc07dd44f1
Andrew Williams <andy@tensixtyone.com>
2010-10-14 10:51:12 +0100

    Fleshed out the SSO admin pages

--- sso/admin.py
+++ sso/admin.py
@@ -5,13 +5,15 @@ from sso.models import Service, ServiceAccount, SSOUser, SSOUserNote
 
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'api', 'active')
-    search_fields = ['name', 'active']
+    search_fields = ['name']
+    list_filter = ('active',)
 
 admin.site.register(Service, ServiceAdmin)
 
 class ServiceAccountAdmin(admin.ModelAdmin):
     list_display = ('service', 'service_uid', 'user', 'active')
     search_fields = ['service_uid', 'user__username']
+    list_filter = ('service', 'active')
 
 admin.site.register(ServiceAccount, ServiceAccountAdmin)
 
@@ -31,6 +33,7 @@ admin.site.register(User, SSOUserAdmin)
 
 class SSOUserNoteAdmin(admin.ModelAdmin):
     list_display = ('user', 'note', 'date_created', 'created_by')
+    search_fields = ['user__username']
 
 admin.site.register(SSOUserNote, SSOUserNoteAdmin)
 
