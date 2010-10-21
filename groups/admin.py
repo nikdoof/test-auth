a52a90bed3461e44bb6b0b682d7eef737eca280e
Andrew Williams <andy@tensixtyone.com>
2010-10-21 17:23:27 +0100

    Add admin panels, integrate Group Information into the admin interface

---
+++ groups/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import GroupAdmin
+from groups.models import GroupInformation, GroupRequest
+
+class GroupRequestAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(GroupRequest, GroupRequestAdmin)
+
+class SSOGroupInformationInline(admin.StackedInline):
+    model = GroupInformation
+    fk_name = 'group'
+    max_num = 1
+
+# Define a new UserAdmin class
+class SSOGroupAdmin(GroupAdmin):
+    inlines = [SSOGroupInformationInline, ]
+
+# Re-register UserAdmin
+admin.site.unregister(Group)
+admin.site.register(Group, SSOGroupAdmin)
+
+
