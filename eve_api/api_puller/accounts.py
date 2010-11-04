f64f31dedada0f9c21bbaea77048fa907d6b4afd
Andrew Williams <andy@tensixtyone.com>
2010-11-04 12:51:11 +0000

    Save the EVEAccount instance before attempting to add characters to it.
    Since Django 1.2, the ORM needs to know what database the object belongs in before additions to their M2M fields. Saving the record before the character import solves the problem.

--- eve_api/api_puller/accounts.py
+++ eve_api/api_puller/accounts.py
@@ -72,14 +72,11 @@ def import_eve_account(api_key, user_id, force_cache=False):
 
     # Create or retrieve the account last to make sure everything
     # before here is good to go.
-    try:
-        account = EVEAccount.objects.get(id=user_id)
-    except EVEAccount.DoesNotExist:
-        account = EVEAccount(id=user_id)
-
+    account, created = EVEAccount.objects.get_or_create(id=user_id)
     account.api_key = api_key
     account.api_user_id = user_id
     account.api_status = API_STATUS_OK
+    account.save()
 
     for node in characters_node_children:
         try:
