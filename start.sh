27d769e7a3a701f73399fa9458a46e07785d01fe
Andrew Williams <andy@tensixtyone.com>
2010-11-08 11:30:39 +0000

    Add celeryd to the startup

--- start.sh
+++ start.sh
@@ -14,3 +14,4 @@ sleep 1
 cd $ROOT
 source ./env/bin/activate
 ./manage.py runfcgi daemonize=true pidfile=$PIDFILE host=127.0.0.1 port=9981 errlog=$ROOT/logs/stderr.log outlog=$ROOT/logs/stdout.log
+./manage.py celeryd -B > $ROOT/logs/celeryd.lg 2>&1 &
