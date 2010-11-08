4df72a3c7f7b36528a7a60463dce24825d0cc713
Andrew Williams <andy@tensixtyone.com>
2010-11-08 12:48:13 +0000

    Remove the RemoveInvalidUsers job, no longer needed

--- sso/cron.py
+++ sso/cron.py
@@ -4,26 +4,6 @@ from django.contrib.auth.models import User, Group
 from eve_api.models import EVEAccount
 from sso.models import ServiceAccount, Service
 
-class RemoveInvalidUsers():
-        """
-        Cycles through all users and runs the permission checking routine for
-        each profile.
-        """
-
-        # run every 2 hours
-        run_every = 7200
-
-        @property
-        def _logger(self):
-            if not hasattr(self, '__logger'):
-                self.__logger = logging.getLogger(__name__)
-            return self.__logger
-                
-        def job(self, args):
-            for user in User.objects.all():
-                # For each user, update access list based on Corp details
-                user.get_profile().update_access()
-
 class UpdateServiceGroups():
         """
         Cycles through all service accounts and updates group access.
