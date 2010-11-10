f7657a73253a72f00c91e45c294bd4d1aa2475d3
Andrew Williams <andy@tensixtyone.com>
2010-11-10 12:01:00 +0000

    Update-directors now works better causing less unneeded task triggering

--- update-directors.py
+++ update-directors.py
@@ -11,23 +11,24 @@ path = os.path.dirname(os.path.realpath( __file__ ))
 activate_this = os.path.join(path, 'env/bin/activate_this.py')
 execfile(activate_this, dict(__file__=activate_this))
 
-import sys
-import logging
 from django.core.management import setup_environ
 import settings
-
 setup_environ(settings)
 
 from eve_api.models import EVEPlayerCharacter
-from django.contrib.auth.models import Group
-import unicodedata
-import re
+from django.contrib.auth.models import Group, User
 
 g = Group.objects.get(name="Alliance Directors")
-c = EVEPlayerCharacter.objects.filter(corporation__alliance__name="Test Alliance Please Ignore",director=True)
 
-for m in g.user_set.all():
+users = []
+for char in EVEPlayerCharacter.objects.filter(corporation__alliance__name="Test Alliance Please Ignore",director=True):
+    users.append(char.eveaccount_set.all()[0].user)
+
+add = set(users) - set(g.user_set.all())
+rem = set(g.user_set.all()) - set(users)
+
+for m in rem:
     m.groups.remove(g)
 
-for char in c:
-    char.eveaccount_set.all()[0].user.groups.add(g)
+for m in add:
+    m.groups.add(g)
