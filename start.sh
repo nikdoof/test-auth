8c4f5c4b8493abbae2b4f23ba26fe10d935d8358
Andrew Williams <andy@tensixtyone.com>
2010-12-02 16:03:42 +0000

    Removed cam startup from start script

--- start.sh
+++ start.sh
@@ -25,4 +25,4 @@ source ./env/bin/activate
 
 ./manage.py celeryd_detach -l INFO -B -E --pidfile $CELERYD_PID_FILE -f $ROOT/logs/celeryd.log -n auth-processor
 ./manage.py runfcgi daemonize=true pidfile=$AUTH_PID_FILE host=127.0.0.1 port=9981 errlog=$ROOT/logs/stderr.log outlog=$ROOT/logs/stdout.log
-./manage.py celerycam --freq=1.0 &
+#./manage.py celerycam --freq=1.0 &
