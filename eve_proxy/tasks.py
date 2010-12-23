0871770756400c3531e28d40a23a215d0d656154
Andrew Williams <andy@tensixtyone.com>
2010-12-23 00:47:41 +0000

    Fixed log cleanup job

--- eve_proxy/tasks.py
+++ eve_proxy/tasks.py
@@ -18,7 +18,7 @@ def clear_stale_cache(cache_extension=0):
 def clear_old_logs():
     log = clear_old_logs.get_logger()
 
-    time = datetime.utcnow() - timedelta(day=settings.EVE_PROXY_KEEP_LOGS)
+    time = datetime.utcnow() - timedelta(days=settings.EVE_PROXY_KEEP_LOGS)
     objs = ApiAccessLog.objects.filter(time_access__lt=time)
     log.info('Removing %s old access logs' % objs.count())
     objs.delete()
