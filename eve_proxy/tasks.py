69742860652f84fccf0bac80f0e72bb03ddf64b1
Andrew Williams <andy@tensixtyone.com>
2011-01-04 08:39:41 +0000

    Fix the log autoclean job

--- eve_proxy/tasks.py
+++ eve_proxy/tasks.py
@@ -2,7 +2,7 @@ from django.conf import settings
 import logging
 from datetime import datetime, timedelta
 from celery.decorators import task
-from eve_proxy.models import CachedDocument
+from eve_proxy.models import CachedDocument, ApiAccessLog
 
 @task(ignore_result=True)
 def clear_stale_cache(cache_extension=0):
