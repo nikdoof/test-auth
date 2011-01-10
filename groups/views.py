61ba8675abfdfba568878d50b0b4050ef32acaa5
Andrew Williams <andy@tensixtyone.com>
2011-01-10 17:22:44 +0000

    Shows if a request is pending

--- groups/views.py
+++ groups/views.py
@@ -51,7 +51,9 @@ def group_list(request):
             requestable = False
 
         fixed = not group.groupinformation.type == GROUP_TYPE_PERMISSION
-        group_list.append((group.id, group.name, status, requestable, fixed))
+	pending = group.requests.filter(status=REQUEST_PENDING,user=request.user).count()
+
+        group_list.append((group.id, group.name, status, requestable, fixed, pending))
 
     return render_to_response('groups/group_list.html', locals(), context_instance=RequestContext(request))
 
