1696ac0d4337801840bfd4f33b8e74aa930d9f80
Andrew Williams <andy@tensixtyone.com>
2011-01-20 09:17:16 +0000

    Add further error checking to the Reddit tasks, gracefully handle issues

--- reddit/tasks.py
+++ reddit/tasks.py
@@ -14,8 +14,8 @@ class send_reddit_message(Task):
         logger = self.get_logger(**kwargs)
 
         logger.info("Sending Reddit message to %s" % to)
-        ib = Inbox(username=settings.REDDIT_USER, password=settings.REDDIT_PASSWORD)
         try:
+            ib = Inbox(username=settings.REDDIT_USER, password=settings.REDDIT_PASSWORD)
             ib.send(to, subject, message)
         except (HTTPError, URLError), exc:
             logger.error("Error sending message, queueing for retry")
@@ -30,21 +30,19 @@ def process_validations():
     logger = process_validations.get_logger()
     try:
         inbox = Inbox(settings.REDDIT_USER, settings.REDDIT_PASSWORD)
+        for msg in inbox:
+            if not msg.was_comment:
+                try:
+                    acc = RedditAccount.objects.get(username__iexact=msg.author)
+                    if not acc.validated and msg.subject == "Validation: %s" % acc.user.username:
+                        logger.info("Validated %s" % acc.user.username)
+                        acc.validated = True
+                        acc.save()
+                except RedditAccount.DoesNotExist:
+                    continue
     except (HTTPError, URLError), exc:
         logger.error("Error with Reddit, aborting.")
         return
     except LoginError, exc:
         logger.error("Error logging into Reddit")
         return 
-
-    for msg in inbox:
-        if not msg.was_comment:
-            try:
-                acc = RedditAccount.objects.get(username__iexact=msg.author)
-                if not acc.validated and msg.subject == "Validation: %s" % acc.user.username:
-                    logger.info("Validated %s" % acc.user.username)
-                    acc.validated = True
-                    acc.save()
-            except RedditAccount.DoesNotExist:
-                continue
-
