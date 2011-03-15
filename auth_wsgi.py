9d91a9c616dc7b463288ee6f9a25a14032fdf9fd
Andrew Williams <andy@tensixtyone.com>
2011-03-15 12:09:55 +0000

    Fix the uwsgi startup scripts and config

--- auth_wsgi.py
+++ auth_wsgi.py
@@ -2,6 +2,6 @@ import sys
 import os
 import django.core.handlers.wsgi
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "app")))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
 application = django.core.handlers.wsgi.WSGIHandler()
