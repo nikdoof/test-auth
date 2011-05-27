814392001f17bf3d4626e0ceea28137e2bd9d37c
Andrew Williams <andy@tensixtyone.com>
2011-05-27 14:53:16 +0100

    Update the WSGI to use production

--- application.py
+++ application.py
@@ -3,5 +3,5 @@ import os
 import django.core.handlers.wsgi
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "app")))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.conf.production'
 application = django.core.handlers.wsgi.WSGIHandler()
