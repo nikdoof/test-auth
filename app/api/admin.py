d5fd90cb5bb6ec6daa9722fe6867f308e127d68b
Andrew Williams <andy@tensixtyone.com>
2011-05-31 13:12:24 +0100

    Disallow add on various models that are readonly

--- app/api/admin.py
+++ app/api/admin.py
@@ -12,5 +12,9 @@ class AuthAPILogAdmin(admin.ModelAdmin):
     list_display = ('key', 'url', 'access_datetime')
     list_filter = ('key',)
 
+    def has_add_permission(self, request):
+        return False
+
+
 admin.site.register(AuthAPIKey, AuthAPIKeyAdmin)
 admin.site.register(AuthAPILog, AuthAPILogAdmin)
--- app/eve_proxy/admin.py
+++ app/eve_proxy/admin.py
@@ -6,11 +6,20 @@ class CachedDocumentAdmin(admin.ModelAdmin):
     list_display = ('url_path', 'time_retrieved', 'cached_until')
     verbose_name = 'Cached Document'
     verbose_name_plural = 'Cached Documents'
+
+    def has_add_permission(self, request):
+        return False
+
 admin.site.register(CachedDocument, CachedDocumentAdmin)
 
 class ApiAccessLogAdmin(admin.ModelAdmin):
     model = ApiAccessLog
     list_display = ('userid', 'service', 'document', 'time_access')
+    list_filter = ('service',)
     verbose_name = 'API Access Log'
     verbose_name_plural = 'API Access Logs'
+
+    def has_add_permission(self, request):
+        return False
+
 admin.site.register(ApiAccessLog, ApiAccessLogAdmin)
