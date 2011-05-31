f973d76c0605b09db63e0b4f87a0bf5a942a41a5
Andrew Williams <andy@tensixtyone.com>
2011-05-31 13:03:29 +0100

    Add some filters to the API admin area

--- app/api/admin.py
+++ app/api/admin.py
@@ -5,11 +5,12 @@ from api.models import AuthAPIKey, AuthAPILog
 class AuthAPIKeyAdmin(admin.ModelAdmin):
     list_display = ('key', 'name', 'url', 'active')
     search_fields = ['name']
+    list_filter = ('active',)
 
 
 class AuthAPILogAdmin(admin.ModelAdmin):
     list_display = ('key', 'url', 'access_datetime')
-
+    list_filter = ('key',)
 
 admin.site.register(AuthAPIKey, AuthAPIKeyAdmin)
 admin.site.register(AuthAPILog, AuthAPILogAdmin)
