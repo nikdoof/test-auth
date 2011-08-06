ce9b7b6ebc6e9e101aca66c4ea7d5e990445338d
Andrew Williams <andy@tensixtyone.com>
2011-08-06 21:23:59 +0100

    Update EDK API export

--- app/tools/management/commands/edkapiextract.py
+++ app/tools/management/commands/edkapiextract.py
@@ -8,14 +8,14 @@ class Command(NoArgsCommand):
     help = "Extracts a list of director's full API keys in CSV format for uploading to EDK"
 
     def handle_noargs(self, **options):
-        chars = EVEPlayerCharacter.objects.filter(roles__name='roleDirector', eveaccount__api_keytype=API_KEYTYPE_FULL, eveaccount__api_status=API_STATUS_OK)
+        chars = EVEPlayerCharacter.objects.filter(roles__name='roleDirector', eveaccount__api_keytype=API_KEYTYPE_FULL, eveaccount__api_status=API_STATUS_OK, corporation__alliance__ticker="TEST")
         donekeys = []
 
         i = 0
         for c in chars:
             i = i + 1
             print "\"TEST\",\"API_CharID_%s\",\"%s\"" % (i, c.id)
-            print "\"TEST\",\"API_UserID_%s\",\"%s\"" % (i, c.eveaccount_set.all()[0].id)
+            print "\"TEST\",\"API_UserID_%s\",\"%s\"" % (i, c.eveaccount_set.all()[0].pk)
             print "\"TEST\",\"API_Key_%s\",\"%s\"" % (i, c.eveaccount_set.all()[0].api_key)
             print "\"TEST\",\"API_Name_%s\",\"%s - %s\"" % (i, c.corporation.name, c.name)
             print "\"TEST\",\"API_Type_%s\",\"corp\"" % (i)
