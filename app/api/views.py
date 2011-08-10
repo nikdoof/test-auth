9592f3b77df0ba572fc73efc885f9a82dd0b0c18
Andrew Williams <andy@tensixtyone.com>
2011-08-10 13:41:57 +0100

    Missing import for redirect

--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
