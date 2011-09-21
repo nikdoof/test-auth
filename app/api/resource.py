52a53fa3c3dbd740fd74adf3c04de6d9f952a794
Andrew Williams <andy@tensixtyone.com>
2011-09-21 16:19:20 +0100

    Fix resource handler

--- app/api/resource.py
+++ app/api/resource.py
@@ -12,4 +12,4 @@ class SentryResource(Resource):
         logger.error('Piston exception: %s(%s)' % (e.__class__.__name__, e), exc_info=sys.exc_info(),
             extra={'data': {'handler': meth.im_class, 'request': request, 'get': dict(request.GET), 'post': dict(request.POST) }})
 
-        return super(SentryResource, self).error_handler(self, e, request, meth, em_format)
+        return super(SentryResource, self).error_handler(e, request, meth, em_format)
