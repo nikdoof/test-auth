bc72a4c83812de063b91aab4a714296c0ccf14f7
Andrew Williams <andy@tensixtyone.com>
2011-09-25 19:35:39 +0100

    Fix blacklist redirect

--- app/hr/views.py
+++ app/hr/views.py
@@ -368,4 +368,4 @@ class HrBlacklistUser(FormView):
 
         update_user_access.delay(user=self.blacklist_user.id)
 
-        return redirect('sso.views.user_view', username=self.blacklist_user.username)
+        return HttpResponseRedirect(reverse('sso.views.user_view', args=[self.blacklist_user.username]))
