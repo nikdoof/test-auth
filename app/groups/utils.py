c6c397dab0e5549ddce59c388dbf0235bc6b44c3
Andrew Williams <andy@tensixtyone.com>
2011-09-30 10:00:54 +0100

    Rename the function, and provide the "domain" context for url linking in the emails

--- app/groups/utils.py
+++ app/groups/utils.py
@@ -2,14 +2,14 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
+from django.contrib.sites.models import Site
 
-def send_group_mail(request, to_email, subject, email_text_template, email_html_template):
+def send_group_email(request, to_email, subject, email_text_template, email_html_template):
     """Sends a email to a group of people using a standard layout"""
 
     # Mail the admins to inform them of a new request
-    ctx = Context({'request': obj})
-    to_email = group.admins.values_list('email', flat=True)
+    ctx = Context({'request': request, 'domain': Site.objects.get_current().domain})
     msg = EmailMultiAlternatives(subject, get_template(email_text_template).render(ctx), getattr(settings, 'DEFAULT_FROM_EMAIL', 'auth@pleaseignore.com'), to_email)
     msg.attach_alternative(get_template(email_html_template).render(ctx), 'text/html')
-    mag.send(fail_silently=True)
+    msg.send(fail_silently=True)
 
