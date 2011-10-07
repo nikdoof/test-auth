2ecbd58702ec29e03b6152bec3a4aff01b119e6a
Andrew Williams <andy@tensixtyone.com>
2011-10-07 09:20:30 +0100

    Export blacklist level via the API

--- app/api/handlers/v1.py
+++ app/api/handlers/v1.py
@@ -225,7 +225,7 @@ class OpTimerHandler(BaseHandler):
 class BlacklistHandler(BaseHandler):
     model = Blacklist
     allowed_methods = ('GET')
-    fields = ('type', 'value', 'source', ('name', 'ticker'), 'created_date', 'expiry_date', 'reason')
+    fields = ('type', 'value', 'level', 'source', ('name', 'ticker'), 'created_date', 'expiry_date', 'reason')
 
     def read(self, request):
         if request.GET.get('value'):
