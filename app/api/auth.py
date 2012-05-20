04328bb0ed84180aa9e5ce7f749eafb1ab96d4fc
Andrew Williams <andy@tensixtyone.com>
2012-05-20 16:12:10 +0100

    Use create instead of instance and save

--- app/api/auth.py
+++ app/api/auth.py
@@ -24,7 +24,7 @@ class APIKeyAuthentication(object):
                     url = "%s?%s" % (request.path, urlencode(params))
                 else:
                     url = request.path
-                AuthAPILog(key=keyobj, access_datetime=now(), url=url).save()
+                AuthAPILog.objects.create(key=keyobj, access_datetime=now(), url=url)
                 request.user = AnonymousUser()
                 request.api_key = keyobj
                 return True
